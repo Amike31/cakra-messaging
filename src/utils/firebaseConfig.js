47e83670341cc809d885313d943e248620cf8de1
Amike31 <rahmatrafid22@gmail.com>
2024-08-02 14:17:21 +0700

    [config] firebase quota
    - prev project was  excedeed
    - relocate new project

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5S_IvwBv_w36yUGDQlAKvJqO-BMpYYX8",
-  authDomain: "cakra-ai-7551a.firebaseapp.com",
-  projectId: "cakra-ai-7551a",
-  storageBucket: "cakra-ai-7551a.appspot.com",
-  messagingSenderId: "716839074984",
-  appId: "1:716839074984:web:38c0c235c746a1176b569d"
+  apiKey: "AIzaSyCogcNwYDYBQDqgacPM6x81nJqjDyExIO8",
+  authDomain: "cakra-messenger-6d711.firebaseapp.com",
+  projectId: "cakra-messenger-6d711",
+  storageBucket: "cakra-messenger-6d711.appspot.com",
+  messagingSenderId: "895916096273",
+  appId: "1:895916096273:web:b1d61de16a68fdfed12ab0"
 };
 
 const app = initializeApp(firebaseConfig);
